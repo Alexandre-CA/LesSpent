400e865368cf82d15b2045ee2d5247618784806e
Alexandre Araujo <80693423+Alexandre-CA@users.noreply.github.com>
2025-02-04 23:39:20 +0000

    :sparkles: feat: install pnpm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-[100vh]">
-      <span className="text-[2rem]">Te amo muito momore ❤️❤️❤️</span>
     </div>
   );
 }
