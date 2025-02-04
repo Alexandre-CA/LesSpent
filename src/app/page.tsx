174560fec3775fe08506db32655c2b59f550c4cd
Alexandre Araujo <80693423+Alexandre-CA@users.noreply.github.com>
2025-02-04 23:33:49 +0000

    :sparkles: feat: .editorconfig

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-[100vh]">
-      <span className="text-[2rem]">
-        Te amo muito momore ❤️❤️❤️
-      </span>
-        </div>
+      <span className="text-[2rem]">Te amo muito momore ❤️❤️❤️</span>
+    </div>
   );
 }
